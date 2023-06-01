57cbe995f8e35c9f01f991721cc17459331db377
hanhafizh <farhanhafizh770@gmail.com>
2023-06-01 20:48:55 +0700

    Membekukan Waktu

--- e2e/Liking_Movies.spec.ts
+++ e2e/Liking_Movies.spec.ts
@@ -11,6 +11,15 @@ Scenario('showing empty liked movies', ({ I }) => {
 
 Scenario('liking one movie', ({ I }) => {
     I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+   
     I.amOnPage('/');
-    // … kita akan mengisi uji coba berikutnya …
+   
+    I.seeElement('.movie__title a');
+    I.click(locate('.movie__title a').first());
+   
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+   
+    I.amOnPage('/#/like');
+    I.seeElement('.movie-item');
   });
